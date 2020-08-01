5f3aef768c3926c30adbb90ba497fb1cd9d26b6f
kajal <you@example.com>
2020-08-01 23:19:22 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import json 
+import os
+
+app = FastAPI()
+
+# Get all data from json file
+@app.get("/Api")
+def read_root():
+        with open("main.json","r") as file:
+            read_data = file.read()
+            data_lode = json.loads(read_data)
+        return (data_lode)
+
+# # Post data into json file
+class Item(BaseModel):
+    name: str
+    description: Optional[str]
+
+@app.post("/items/")
+async def create_item(item: Item):
+    with open("main.json","r") as file:
+        read_data = file.read()
+        data_lode = json.loads(read_data)
+        id = len(data_lode)
+        data = {
+            "id": id+1,
+            "name" : item.name,
+            "description" : item.description
+        }
+        data_lode.append(data)
+        # print(data_lode)
+        with open("main.json", "w") as outfile: 
+            json.dump(data_lode, outfile) 
+
+#  update your data from json file
+@app.put("/items/{id}")
+async def create_item(id : int ,item: Item):
+    # print(id)
+    with open("main.json","r") as file:
+        read_data = file.read()
+        data_lode = json.loads(read_data)
+        index = (data_lode[id-1])
+        data_lode.pop(id-1)
+        index = {
+            "id" :id,
+            "name" : item.name,
+            "description" : item.description
+        }
+        data_lode.append(index)
+        print(data_lode)
+        with open("main.json", "w") as outfile: 
+            json.dump(data_lode, outfile) 
